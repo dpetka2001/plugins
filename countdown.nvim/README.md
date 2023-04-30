704f0b057ae8097bd621d1dbb1a088a3d150021b
Iordanis Petkakis <dpetka2001@gmail.com>
2023-04-30 14:07:34 +0300

    feat(countdown): add `CountdownCancel` command and functionality

---
+++ countdown.nvim/README.md
@@ -0,0 +1,110 @@
+# Countdown Timer for Neovim
+
+This plugin provides a countdown timer for Neovim that can be started, stopped, resumed, and reset with either lua functions or user commands. The remaining time is displayed in the format `hh:mm:ss`.
+
+**Motivation**: My motivation for creating this plugin was to have an easy-to-use countdown timer for Pomodoro technique sessions in Nvim.
+
+## Installation
+
+```lua
+  use { 'cbrgm/countdown.nvim', config = function()
+    require("countdown").setup({
+      default_minutes = 25, -- The default minutes to use when a countdown is started without minutes specified
+    })
+  end }
+```
+
+## Example usage with Lualine
+
+Sample usage displaying the countdown with [lualine.nvim](https://github.com/nvim-lualine/lualine.nvim)
+
+```lua
+-- function returns a formatted string hh:mm:ss
+local timer = function()
+	return require("countdown").get_time()
+end
+
+lualine.setup {
+	sections = {},
+	inactive_sections = {},
+	tabline = {
+	        -- add the timer to the tabline bar
+		lualine_x = { timer, "encoding", "fileformat", "filetype" },
+	},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {},
+}
+```
+
+## Keybindings
+
+Sample key bindings:
+
+```lua
+vim.api.nvim_set_keymap('n', '<leader>ttn', function()
+	require("countdown").start_countdown(25)
+end, { desc = "Start Pomodoro session" })
+
+vim.api.nvim_set_keymap('n', '<leader>ttb', function()
+	require("countdown").start_countdown(5)
+end, { desc = "Start Pomodoro break" })
+
+vim.api.nvim_set_keymap('n', '<leader>tts', function()
+	require("countdown").stop_countdown()
+end, { desc = "Stop Countdown" })
+
+vim.api.nvim_set_keymap('n', '<leader>ttr', function()
+	require("countdown").resume_countdown()
+end, { desc = "Resume Countdown" })
+```
+
+## Lua Functions
+
+The following lua functions are available for starting, stopping, resuming, and resetting the countdown timer:
+
+```lua
+local countdown = require('countdown')
+
+-- Starts/Resets the countdown with a specified number of minutes
+countdown.start_countdown(minutes)
+
+-- Stop the countdown if it is currently running
+countdown.stop_countdown()
+
+-- Resume the countdown if it is currently stopped
+countdown.resume_countdown()
+
+-- Get the countdowns time as string in format hh:mm:ss
+countdown.get_time()
+```
+
+## User Commands
+
+The following user commands are available for starting, stopping, resuming, and resetting the countdown timer:
+
+```
+" Start/Reset the countdown for a specified number of minutes
+:CountdownStart minutes
+
+" Stop the countdown if it is currently running
+:CountdownStop
+
+" Resume the countdown if it is currently stopped
+:CountdownResume
+
+```
+
+## Default Configuration
+
+You can configure the default number of minutes to use when starting the countdown with a value less than or equal to zero.
+
+```lua
+countdown.setup({
+  default_minutes = 25, -- The default minutes to use when a countdown is started without specifing minutes
+})
+```
+
+## License
+
+This plugin is licensed under the MIT License. See the LICENSE file for details.
---
+++ countdown.nvim/lua/countdown/init.lua
@@ -0,0 +1,134 @@
+local M = {}
+
+-- Declare variables for the timer, seconds remaining in the countdown, and the configuration.
+local timer
+local seconds_remaining = 0
+local config = {
+  default_minutes = 25,
+}
+
+-- Formats a given number of seconds into hours, minutes, and seconds.
+-- @param seconds The number of seconds to format.
+-- @return A string in the format "HH:MM:SS".
+local function format_time(seconds)
+  local hours = string.format("%02d", math.floor(seconds / 3600))
+  local minutes = string.format("%02d", math.floor((seconds % 3600) / 60))
+  local seconds = string.format("%02d", seconds % 60)
+  return hours .. ":" .. minutes .. ":" .. seconds
+end
+
+-- Decrements the seconds remaining and stops the timer if necessary.
+local function countdown_callback()
+  seconds_remaining = seconds_remaining - 1
+  if seconds_remaining <= 0 then
+    print("Time is up!")
+    timer:stop()
+    timer = nil
+  end
+end
+
+-- Gets the current time remaining in the countdown.
+-- @return A string in the format "HH:MM:SS" if the countdown is ongoing, or nil if the countdown is not ongoing.
+local function get_time()
+  if seconds_remaining > 0 then
+    return format_time(seconds_remaining)
+  end
+  return ""
+end
+
+-- Starts a countdown with the given number of minutes.
+-- If the minutes parameter is not a positive integer, the default number of minutes specified in the config will be used instead.
+-- If the default number of minutes is not set or is not a positive integer, an error message will be printed.
+-- @param minutes The number of minutes for the countdown.
+local function start_countdown(minutes)
+  if minutes == nil then
+    minutes = config.default_minutes
+  end
+  if minutes <= 0 and config.default_minutes then
+    minutes = config.default_minutes
+  end
+  if minutes <= 0 then
+    print("Error: Countdown duration must be a positive integer.")
+    return
+  end
+  seconds_remaining = minutes * 60
+  timer = vim.loop.new_timer()
+  timer:start(1000, 1000, countdown_callback)
+end
+
+-- Cancels the current countdown
+local function cancel_countdown()
+  if timer then
+    seconds_remaining = 0
+    timer:stop()
+    timer = nil
+  end
+end
+
+-- Stops the current countdown.
+local function stop_countdown()
+  if timer then
+    timer:stop()
+    timer = nil
+  end
+end
+
+-- Resumes the current countdown if it has been paused.
+local function resume_countdown()
+  if not timer and seconds_remaining > 0 then
+    timer = vim.loop.new_timer()
+    timer:start(1000, 1000, countdown_callback)
+  end
+end
+
+-- Resets the current countdown with the given number of minutes.
+-- If the minutes parameter is not a positive integer, the default number of minutes specified in the config will be used instead.
+-- If the default number of minutes is not set or is not a positive integer, an error message will be printed.
+-- @param minutes The number of minutes for the countdown.
+local function resetCountdown(minutes)
+  stop_countdown()
+  start_countdown(minutes)
+end
+
+-- Expose the stop_countdown, resume_countdown, start_countdown, and get_time functions as methods of the M table.
+M.stop_countdown = stop_countdown
+M.resume_countdown = resume_countdown
+M.start_countdown = resetCountdown
+M.get_time = get_time
+M.config = config
+M.cancel_countdown = cancel_countdown
+
+-- Registers the four commands "CountdownStop", "CountdownResume", "CountdownStart", and "CountdownTime" with Neovim.
+-- The "CountdownStart" command takes an optional argument for the number of minutes in the countdown.
+-- If no argument is provided, the default number of minutes specified in the config will be used instead.
+function M.setup(c)
+
+  vim.api.nvim_create_user_command("CountdownStop", function()
+    M.stop_countdown()
+  end, { nargs = 0 })
+
+  vim.api.nvim_create_user_command("CountdownCancel", function()
+    M.cancel_countdown()
+  end, { nargs = 0 })
+
+  vim.api.nvim_create_user_command("CountdownResume", function()
+    M.resume_countdown()
+  end, { nargs = 0 })
+
+  vim.api.nvim_create_user_command("CountdownStart", function(args)
+    local minutes = args["args"] or nil
+    if minutes == nil then
+      M.start_countdown(M.config.default_minutes)
+    else
+      M.start_countdown(tonumber(minutes))
+    end
+  end, { nargs = '?' })
+
+  vim.api.nvim_create_user_command("CountdownTime", function()
+    print(M.get_time())
+  end, { nargs = 0 })
+  config = vim.tbl_deep_extend('force', config, c)
+end
+
+-- Return the M table.
+return M
